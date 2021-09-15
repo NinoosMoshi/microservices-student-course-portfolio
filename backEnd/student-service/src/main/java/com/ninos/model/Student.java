fa4dce4d712d36ffe83e8d9cfc01201945f8d3a5
NinoosMoshi <nalkaton@gmail.com>
2021-09-15 19:53:02 -0400

    Spring (application.properties, Student, Role) Closes #1

---
+++ backEnd/student-service/src/main/java/com/ninos/model/Student.java
@@ -0,0 +1,30 @@
+package com.ninos.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "student")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "role")
+    private Role role;
+
+
+}
